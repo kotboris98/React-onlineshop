b7743cb9bdc6b872c7473f2e33b278f2abec5037
Boris <borisvip169@gmail.com>
2024-11-06 16:13:18 +0300

    Initial commit

--- onlineshop/src/App.js
+++ onlineshop/src/App.js
@@ -1,7 +1,60 @@
-function App() {
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import React from "react";
+import Items from "./components/Items";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [
+        {
+          id: 1,
+          title: 'Стул',
+          desc: 'Коричневый, деревянный',
+          category: 'chairs',
+          price: '49.99$'
+        },
+        {
+          id: 2,
+          title: 'Стол',
+          desc: 'Круглый, прочный',
+          category: 'chairs',
+          price: '69.99$'
+        },
+        {
+          id: 3,
+          title: 'Кресло',
+          desc: 'Мягкое, удобное',
+          category: 'chairs',
+          price: '79.99$'
+        },
+        {
+          id: 4,
+          title: 'Табурет',
+          desc: 'Квадратный, фанерный',
+          category: 'chairs',
+          price: '19.99$'
+        },
+        {
+          id: 5,
+          title: 'Стеллаж',
+          desc: 'Из 4 полок',
+          category: 'chairs',
+          price: '99.99$'
+        },
+      ],
+    }
+  }
+  render() {
   return (
-    <div>Shop</div> 
+    <div className="wrapper">
+      <Header />
+      <Items items={this.state.items}/>
+      <Footer />
+    </div> 
   );
+}
 }
 
 export default App;
---
+++ onlineshop/src/components/Items.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Items extends Component {
+  render() {
+    return (
+      <main>Items</main>
+    )
+  }
+}
+
+export default Items
